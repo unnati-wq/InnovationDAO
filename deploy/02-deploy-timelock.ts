a6266f218b846028e4f688fb7ddc8c1a12529c85
Unnati Udaipuria <unnatiudaipuria@Unnatis-MacBook-Pro.local>
2023-11-23 19:24:09 +0530

    first commit

---
+++ deploy/02-deploy-timelock.ts
@@ -0,0 +1,24 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { EXECUTORS, MIN_DELAY, PROPOSERS } from "../hardhat-helper-config";
+
+const deployTimeLock: DeployFunction = async (
+  hre: HardhatRuntimeEnvironment
+) => {
+  const { getNamedAccounts, deployments, network } = hre;
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  log("Deploying the TimeLock contract...");
+  const timeLock = await deploy("TimeLock", {
+    from: deployer,
+    args: [MIN_DELAY, PROPOSERS, EXECUTORS],
+    log: true,
+    // waitConfirmations: 1,
+  });
+
+  log(`02-Deployed 'TimeLock' at ${timeLock.address}`);
+};
+
+export default deployTimeLock;
+deployTimeLock.tags = ["all", "timelock"];
---
+++ hardhat.config.ts
@@ -0,0 +1,50 @@
+
+import "@typechain/hardhat";
+import "hardhat-deploy";
+import "@nomiclabs/hardhat-ethers";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+// module.exports = {
+// solidity: {
+//   version: "0.8.9",
+//   settings: {
+//     optimizer: {
+//       enabled: true,
+//       runs: 200,
+//     },
+//   },
+// },
+// };
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    // DEVELOPMENT BLOCKCHAINS
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // INDEX for the deployer's account in the array of accounts that Hardhat gives us.
+    },
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
+
+export default config;
